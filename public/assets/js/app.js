a44425160cd73a6a23c94ac58109cd8b2f35498d
Moh-Elayali <agosmart@gmail.com>
2021-04-10 22:53:42 +0200

    Update CSS

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -37450,10 +37450,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/custom.scss":
-/*!************************************!*\
-  !*** ./resources/sass/custom.scss ***!
-  \************************************/
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -37462,15 +37462,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!********************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/less/style.less ./resources/sass/custom.scss ***!
-  \********************************************************************************************/
+/*!*****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/less/style.less ./resources/sass/app.scss ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\ghtt\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\laragon\www\ghtt\resources\less\style.less */"./resources/less/style.less");
-module.exports = __webpack_require__(/*! D:\laragon\www\ghtt\resources\sass\custom.scss */"./resources/sass/custom.scss");
+__webpack_require__(/*! G:\laragon\www\ghtt\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! G:\laragon\www\ghtt\resources\less\style.less */"./resources/less/style.less");
+module.exports = __webpack_require__(/*! G:\laragon\www\ghtt\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
